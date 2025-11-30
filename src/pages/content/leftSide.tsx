709fc8fd6c53bee62a6cbdeedd02ce06a21eaf93
Waleed Alnaimi <waleedalnaimi117@gmail.com>
2025-12-01 01:59:28 +0200

    added buttons and switching between the left side content

--- src/pages/content/leftSide.tsx
+++ src/pages/content/leftSide.tsx
@@ -1,8 +1,34 @@
+import { useState } from "react";
+import { aboutMeText } from "../aboutMe";
+const introText = `Hello! I'm Waleed, a passionate`;
+const introText2 = ` software developer.`;
+const lineBreak = <svg width="100%" height="4">
+  <rect width="100%" height="4" fill="#52cbbe67" />
+</svg>;
+
 export function LeftSide() {
+  const [page, setPage] = useState("aboutMe");
+  let pContent;
+  if (page === "aboutMe") {
+    pContent = aboutMeText;
+  } else if (page === "projects") {
+    pContent = "Projects";
+  } else if (page === "skills") {
+    pContent = "Skills";
+  }
   return (
     <div className="leftSide">
         <div className="leftSide__inner">
-            <h1 className="leftSide__h">Left Side</h1>
+            <div className="leftSide__inner_upper">
+            <h1 className="leftSide__h">{introText}<strong>{introText2}</strong></h1>
+            {lineBreak}
+            <p className="leftSide__p">{pContent}</p>
+          </div>
+          <div className="leftSide__inner_lower">
+            <button className="leftSide__btn" onClick={() => setPage("aboutMe")}>About me</button>
+            <button className="leftSide__btn" onClick={() => setPage("projects")}>Projects</button>
+            <button className="leftSide__btn" onClick={() => setPage("skills")}>Skills</button>
+          </div>
         </div>
     </div>
   );
