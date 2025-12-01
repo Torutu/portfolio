f76f141671a2d36af4a5a1f0f2c7bbbac08846bc
Waleed Alnaimi <waleedalnaimi117@gmail.com>
2025-12-01 04:50:52 +0200

    improved project menu

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -10,16 +10,56 @@ export function LeftProjectsText() {
         projects, I enjoy pushing forward and learn along the way
       </p>
       <div className="leftSide__menu">
-        <div className="leftSide__menu_item" onClick={() => setSelectedProject("project1")}>Project 1</div>
-        <div className="leftSide__menu_item" onClick={() => setSelectedProject("project2")}>Project 2</div>
-        <div className="leftSide__menu_item" onClick={() => setSelectedProject("project3")}>Project 3</div>
-        <div className="leftSide__menu_item" onClick={() => setSelectedProject("project4")}>Project 4</div>
-        <div className="leftSide__menu_item" onClick={() => setSelectedProject("project5")}>Project 5</div>
-        <div className="leftSide__menu_item" onClick={() => setSelectedProject("project6")}>Project 6</div>
-        <div className="leftSide__menu_item" onClick={() => setSelectedProject("project7")}>Project 7</div>
-        <div className="leftSide__menu_item" onClick={() => setSelectedProject("project8")}>Project 8</div>
-        <div className="leftSide__menu_item" onClick={() => setSelectedProject("project9")}>Project 9</div>
-        <div className="leftSide__menu_item" onClick={() => setSelectedProject("project10")}>Project 10</div>
+        <div className="leftSide__menu_item" onClick={() => setSelectedProject("project1")}>
+            <div className="leftSide__menu_item_name">Project 1</div>
+            <div className="leftSide__menu_item_description">Description</div>
+            <div className="leftSide__menu_item_badges">Badges</div>
+        </div>
+        <div className="leftSide__menu_item" onClick={() => setSelectedProject("project2")}>
+            <div className="leftSide__menu_item_name">Project 2</div>
+            <div className="leftSide__menu_item_description">Description</div>
+            <div className="leftSide__menu_item_badges">Badges</div>
+        </div>
+        <div className="leftSide__menu_item" onClick={() => setSelectedProject("project3")}>
+            <div className="leftSide__menu_item_name">Project 3</div>
+            <div className="leftSide__menu_item_description">Description</div>
+            <div className="leftSide__menu_item_badges">Badges</div>
+        </div>
+        <div className="leftSide__menu_item" onClick={() => setSelectedProject("project4")}>
+            <div className="leftSide__menu_item_name">Project 4</div>
+            <div className="leftSide__menu_item_description">Description</div>
+            <div className="leftSide__menu_item_badges">Badges</div>
+        </div>
+        <div className="leftSide__menu_item" onClick={() => setSelectedProject("project5")}>
+            <div className="leftSide__menu_item_name">Project 5</div>
+            <div className="leftSide__menu_item_description">Description</div>
+            <div className="leftSide__menu_item_badges">Badges</div>
+        </div>
+        <div className="leftSide__menu_item" onClick={() => setSelectedProject("project6")}>
+            <div className="leftSide__menu_item_name">Project 6</div>
+            <div className="leftSide__menu_item_description">Description</div>
+            <div className="leftSide__menu_item_badges">Badges</div>
+        </div>
+        <div className="leftSide__menu_item" onClick={() => setSelectedProject("project7")}>
+            <div className="leftSide__menu_item_name">Project 7</div>
+            <div className="leftSide__menu_item_description">Description</div>
+            <div className="leftSide__menu_item_badges">Badges</div>
+        </div>
+        <div className="leftSide__menu_item" onClick={() => setSelectedProject("project8")}>
+            <div className="leftSide__menu_item_name">Project 8</div>
+            <div className="leftSide__menu_item_description">Description</div>
+            <div className="leftSide__menu_item_badges">Badges</div>
+        </div>
+        <div className="leftSide__menu_item" onClick={() => setSelectedProject("project9")}>
+            <div className="leftSide__menu_item_name">Project 9</div>
+            <div className="leftSide__menu_item_description">Description</div>
+            <div className="leftSide__menu_item_badges">Badges</div>
+        </div>
+        <div className="leftSide__menu_item" onClick={() => setSelectedProject("project10")}>
+            <div className="leftSide__menu_item_name">Project 10</div>
+            <div className="leftSide__menu_item_description">Description</div>
+            <div className="leftSide__menu_item_badges">Badges</div>
+        </div>
       </div>
     </>
   );
